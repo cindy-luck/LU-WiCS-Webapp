b587d006a9289f2b29f7d91d180e8d4535c51f44
Cyndy-Meng <yim222@lehigh.edu>
2021-03-26 02:43:05 -0400

    First commit

---
+++ src/pages/news.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const news = () => (
+  <Layout>
+    <SEO title="News Page" />
+    <h1>New event is coming up soon...</h1>
+  </Layout>
+)
+
+export default news
